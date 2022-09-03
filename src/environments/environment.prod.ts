ac119f4cda8cd54939d22196c9e72b762c0a7c44
Jacob Ambrose <jambrose@worldcubeassociation.org>
2022-09-03 15:08:11 -0400

    set up contentful

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  contentful: {
+    space: 'bat2653uq10o',
+    accessToken: '_dNWWUaVsfA-mXK0xJ25pSti7mHBb7-VXT32swvjx-E'
+  }
 };
