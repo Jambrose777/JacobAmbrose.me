020cb80aad588ba35659e8fbab1cc61293bc3eaf
Jacob Ambrose <jambrose@worldcubeassociation.org>
2022-09-06 12:37:55 -0400

    added education content

--- src/app/components/pages/education/education.component.ts
+++ src/app/components/pages/education/education.component.ts
@@ -6,6 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education.component.scss']
 })
 export class EducationComponent implements OnInit {
+  courses: string[] = [
+    'Algorithms',
+    'Graph Theory', 
+    'Database Management',
+    'Data Structures', 
+    'Computing Architecture',
+    'Computing Theory',
+    'Combinatorics',
+    'Software Development',
+    'Software Enigneering',
+    'Systems Programming',
+    'Computing Networks',
+    'Operating Systems',
+    'Numerical Analysis',
+    'Sequences and Series',
+    'Probability',
+    'Linear Algebra',
+  ];
 
   constructor() { }
 
